496f0217a1293d0a7929759e2750ccfdd5cfeae1
st0012 <stan001212@gmail.com>
2019-10-19 16:54:21 +0800

    Remove uesless test case

--- spec/tapping_device_spec.rb
+++ spec/tapping_device_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe TappingDevice do
   it "has a version number" do
     expect(TappingDevice::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
