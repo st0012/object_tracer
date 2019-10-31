ac45957399113e89d364d9afcd04c1e3e52879a6
st0012 <stan001212@gmail.com>
2019-10-31 18:26:33 +0800

    Store calls in device

--- spec/tapping_device_spec.rb
+++ spec/tapping_device_spec.rb
@@ -1,64 +1,41 @@
 require "spec_helper"
 
 RSpec.describe TappingDevice::Device do
-  describe "#tap_initialization_of!" do
+  describe "#tap_init!" do
+    let(:device) { TappingDevice::Device.new }
+
+    after do
+      device.stop!
+    end
+
     it "tracks Student's initialization" do
-      count = 0
-
-      device = TappingDevice::Device.new do
-        count += 1
-      end
-
       device.tap_init!(Student)
 
       Student.new("Stan", 18)
       Student.new("Jane", 23)
 
-      expect(count).to eq(2)
-
-      device.stop!
+      expect(device.calls.count).to eq(2)
     end
     it "can track subclass's initialization as well" do
-      count = 0
-
-      device = TappingDevice::Device.new do
-        count += 1
-      end
-
       device.tap_init!(HighSchoolStudent)
 
       HighSchoolStudent.new("Stan", 18)
 
-      expect(count).to eq(1)
-      device.stop!
+      expect(device.calls.count).to eq(1)
     end
     it "doesn't track School's initialization" do
-      count = 0
-
-      device = TappingDevice::Device.new do
-        count += 1
-      end
-
       device.tap_init!(Student)
 
       School.new("A school")
 
-      expect(count).to eq(0)
-      device.stop!
+      expect(device.calls.count).to eq(0)
     end
     it "doesn't track non-initialization method calls" do
-      count = 0
-
-      device = TappingDevice::Device.new do
-        count += 1
-      end
-
       device.tap_init!(Student)
 
       Student.foo
 
-      expect(count).to eq(0)
-      device.stop!
+      expect(device.calls.count).to eq(0)
     end
   end
 end
