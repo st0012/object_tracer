18fb1f12137c52694640ff5ef9819d9e221aeb22
st0012 <stan001212@gmail.com>
2019-12-29 15:02:13 +0800

    Move tap_init! related specs into their own file

--- spec/methods/tap_init_spec.rb
+++ spec/methods/tap_init_spec.rb
@@ -1,49 +1,47 @@
 require "spec_helper"
 require "shared_examples/stoppable_examples"
+require "shared_examples/optionable_examples"
 
-RSpec.describe TappingDevice::Trackable do
-  include described_class
+RSpec.describe "tap_init!" do
+  include TappingDevice::Trackable
 
-  describe "#tap_init!" do
-    let(:calls) { [] }
-    subject { :tap_init! }
+  subject { :tap_init! }
+  let(:target) { Student }
+  let(:trigger_action) do
+    -> (target) { target.new("Stan", 18) }
+  end
 
-    it "tracks Student's initialization" do
-      device = tap_init!(Student)
+  it_behaves_like "stoppable"
+  it_behaves_like "optionable"
 
-      Student.new("Stan", 18)
-      Student.new("Jane", 23)
+  it "tracks Student's initialization" do
+    device = tap_init!(Student)
 
-      expect(device.calls.count).to eq(2)
-    end
-    it "can track subclass's initialization as well" do
-      device = tap_init!(HighSchoolStudent)
+    Student.new("Stan", 18)
+    Student.new("Jane", 23)
 
-      HighSchoolStudent.new("Stan", 18)
+    expect(device.calls.count).to eq(2)
+  end
+  it "can track subclass's initialization as well" do
+    device = tap_init!(HighSchoolStudent)
 
-      expect(device.calls.count).to eq(1)
-      expect(device.calls.first.target).to eq(HighSchoolStudent)
-    end
-    it "doesn't track School's initialization" do
-      device = tap_init!(Student)
+    HighSchoolStudent.new("Stan", 18)
 
-      School.new("A school")
+    expect(device.calls.count).to eq(1)
+    expect(device.calls.first.target).to eq(HighSchoolStudent)
+  end
+  it "doesn't track School's initialization" do
+    device = tap_init!(Student)
 
-      expect(device.calls.count).to eq(0)
-    end
-    it "doesn't track non-initialization method calls" do
-      device = tap_init!(Student)
+    School.new("A school")
 
-      Student.foo
+    expect(device.calls.count).to eq(0)
+  end
+  it "doesn't track non-initialization method calls" do
+    device = tap_init!(Student)
 
-      expect(device.calls.count).to eq(0)
-    end
+    Student.foo
 
-    it_behaves_like "stoppable" do
-      let(:target) { Student }
-      let(:trigger_action) do
-        -> (target) { target.new("Stan", 18) }
-      end
-    end
+    expect(device.calls.count).to eq(0)
   end
 end
--- spec/tapping_device_spec.rb
+++ spec/tapping_device_spec.rb
@@ -115,48 +115,6 @@ RSpec.describe TappingDevice do
       expect(call.line_number).to eq(line_1.to_s)
     end
   end
-  describe "#tap_init!" do
-    let(:device) { described_class.new }
-    subject { :tap_init! }
-
-    it "tracks Student's initialization" do
-      device.tap_init!(Student)
-
-      Student.new("Stan", 18)
-      Student.new("Jane", 23)
-
-      expect(device.calls.count).to eq(2)
-    end
-    it "can track subclass's initialization as well" do
-      device.tap_init!(HighSchoolStudent)
-
-      HighSchoolStudent.new("Stan", 18)
-
-      expect(device.calls.count).to eq(1)
-      expect(device.calls.first.target).to eq(HighSchoolStudent)
-    end
-    it "doesn't track School's initialization" do
-      device.tap_init!(Student)
-
-      School.new("A school")
-
-      expect(device.calls.count).to eq(0)
-    end
-    it "doesn't track non-initialization method calls" do
-      device.tap_init!(Student)
-
-      Student.foo
-
-      expect(device.calls.count).to eq(0)
-    end
-
-    it_behaves_like "stoppable" do
-      let(:target) { Student }
-      let(:trigger_action) do
-        -> (target) { target.new("Stan", 18) }
-      end
-    end
-  end
   describe "#and_print" do
     let(:device) { described_class.new }
 
