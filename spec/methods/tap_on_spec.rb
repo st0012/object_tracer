2dc537c04f0ffbd65caa1456d16a71b49af3d88e
st0012 <stan001212@gmail.com>
2019-12-29 15:00:02 +0800

    Move tap_on! related specs into their own file

--- spec/methods/tap_on_spec.rb
+++ spec/methods/tap_on_spec.rb
@@ -1,115 +1,81 @@
 require "spec_helper"
 require "shared_examples/stoppable_examples"
+require "shared_examples/optionable_examples"
 
-RSpec.describe TappingDevice::Trackable do
-  include described_class
+RSpec.describe "tap_on!" do
+  include TappingDevice::Trackable
+  let(:subject) { :tap_on! }
 
-  describe "#tap_on!" do
-    subject { :tap_on! }
+  it_behaves_like "stoppable" do
+    let(:target) { Student.new("Stan", 18) }
+    let(:trigger_action) do
+      -> (target) { target.name }
+    end
+  end
 
-    it_behaves_like "stoppable" do
-      let(:target) { Student.new("Stan", 18) }
-      let(:trigger_action) do
-        -> (target) { target.name }
+  it_behaves_like "optionable" do
+    let(:target) { Student.new("Stan", 18) }
+    let(:trigger_action) do
+      -> (target) { target.name }
+    end
+  end
+
+  it "tracks method calls on the tapped object" do
+    stan = Student.new("Stan", 18)
+    jane = Student.new("Jane", 23)
+
+    device = tap_on!(stan)
+
+    stan.name
+    stan.age
+    jane.name
+    jane.age
+
+    call = device.calls.first
+    expect(call.receiver.object_id).to eq(stan.object_id)
+    expect(call.method_name).to eq(:name)
+    expect(call.return_value).to eq("Stan")
+
+    call = device.calls.last
+    expect(call.receiver.object_id).to eq(stan.object_id)
+    expect(call.method_name).to eq(:age)
+    expect(call.return_value).to eq(18)
+  end
+  it "tracks alias" do
+    c = Class.new(Student)
+    c.class_eval do
+      alias :alias_name :name
+    end
+    stan = c.new("Stan", 18)
+
+    device = tap_on!(stan)
+
+    stan.alias_name
+
+    expect(device.calls.first.method_name).to eq(:alias_name)
+  end
+
+  context "when targets are ActiveRecord::Base instances" do
+    context "with track_as_records: true" do
+      it "tracks ActiveRecord::Base instances with their ids" do
+        post = Post.create!(title: "foo", content: "bar")
+
+        device = tap_on!(post, exclude_by_paths: [/gems/], track_as_records: true)
+
+        Post.last.title
+
+        expect(device.calls.count).to eq(1)
       end
     end
+    context "without track_as_records: true" do
+      it "treats the record like normal objects" do
+        post = Post.create!(title: "foo", content: "bar")
 
-    it "tracks method calls on the tapped object" do
-      stan = Student.new("Stan", 18)
-      jane = Student.new("Jane", 23)
+        device = tap_on!(post, exclude_by_paths: [/gems/])
 
-      device = tap_on!(stan)
-
-      stan.name
-      stan.age
-      jane.name
-      jane.age
-
-      call = device.calls.first
-      expect(call.receiver.object_id).to eq(stan.object_id)
-      expect(call.method_name).to eq(:name)
-      expect(call.return_value).to eq("Stan")
-
-      call = device.calls.last
-      expect(call.receiver.object_id).to eq(stan.object_id)
-      expect(call.method_name).to eq(:age)
-      expect(call.return_value).to eq(18)
-    end
-    it "tracks alias" do
-      c = Class.new(Student)
-      c.class_eval do
-        alias :alias_name :name
-      end
-      stan = c.new("Stan", 18)
-
-      device = tap_on!(stan)
-
-      stan.alias_name
-
-      expect(device.calls.first.method_name).to eq(:alias_name)
-    end
-
-    context "when targets are ActiveRecord::Base instances" do
-      context "with track_as_records: true" do
-        it "tracks ActiveRecord::Base instances with their ids" do
-          post = Post.create!(title: "foo", content: "bar")
-
-          device = tap_on!(post, exclude_by_paths: [/gems/], track_as_records: true)
-
-          Post.last.title
-
-          expect(device.calls.count).to eq(1)
-        end
-      end
-      context "without track_as_records: true" do
-        it "treats the record like normal objects" do
-          post = Post.create!(title: "foo", content: "bar")
-
-          device = tap_on!(post, exclude_by_paths: [/gems/])
-
-          Post.last.title
-
-          expect(device.calls.count).to eq(0)
-        end
-      end
-    end
-
-    describe "options - with_trace_to: 5" do
-      it "stores trace until given index" do
-        stan = Student.new("Stan", 18)
-
-        device = tap_on!(stan, with_trace_to: 5)
-
-        stan.name
-
-        expect(device.calls.first.trace.length).to eq(6)
-      end
-    end
-    describe "options - exclude_by_paths: [/path/]" do
-      it "skips calls that matches the pattern" do
-        stan = Student.new("Stan", 18)
-
-        device = tap_on!(stan, exclude_by_paths: [/spec/])
-
-        stan.name
+        Post.last.title
 
         expect(device.calls.count).to eq(0)
-      end
-    end
-    describe "options - filter_by_paths: [/path/]" do
-      it "skips calls that doesn't match the pattern" do
-        stan = Student.new("Stan", 18)
-        count = 0
-
-        tap_on!(stan, filter_by_paths: [/lib/]) { count += 1 }
-
-        stan.name
-        expect(count).to eq(0)
-
-        tap_on!(stan, filter_by_paths: [/spec/]) { count += 1 }
-
-        stan.name
-        expect(count).to eq(1)
       end
     end
   end
--- spec/tapping_device_spec.rb
+++ spec/tapping_device_spec.rb
@@ -1,6 +1,23 @@
 require "spec_helper"
 
 RSpec.describe TappingDevice do
+  it "supports multiple tappings" do
+    stan = Student.new("Stan", 18)
+
+    count_1 = 0
+    count_2 = 0
+
+    device_1 = described_class.new { count_1 += 1 }
+    device_2 = described_class.new { count_2 -= 1 }
+
+    device_1.tap_on!(stan)
+    device_2.tap_on!(stan)
+
+    stan.name
+
+    expect(count_1).to eq(1)
+    expect(count_2).to eq(-1)
+  end
   describe "tap_passed!"do
     let(:device) { described_class.new }
     subject { :tap_passed! }
@@ -140,187 +157,6 @@ RSpec.describe TappingDevice do
       end
     end
   end
-
-  describe "#tap_on!" do
-    subject { :tap_on! }
-
-    let(:device) do
-      described_class.new do |payload|
-        [payload[:receiver].object_id, payload[:method_name], payload[:return_value]]
-      end
-    end
-
-    it_behaves_like "stoppable" do
-      let(:target) { Student.new("Stan", 18) }
-      let(:trigger_action) do
-        -> (target) { target.name }
-      end
-    end
-
-    it "tracks method calls on the tapped object" do
-      stan = Student.new("Stan", 18)
-      jane = Student.new("Jane", 23)
-
-      device.tap_on!(stan)
-
-      stan.name
-      stan.age
-      jane.name
-      jane.age
-
-      expect(device.calls).to match_array(
-        [
-          [stan.object_id, :name, "Stan"],
-          [stan.object_id, :age, 18]
-        ]
-      )
-    end
-    it "supports multiple tappings" do
-      stan = Student.new("Stan", 18)
-
-      count_1 = 0
-      count_2 = 0
-
-      device_1 = described_class.new { count_1 += 1 }
-      device_2 = described_class.new { count_2 -= 1 }
-
-      device_1.tap_on!(stan)
-      device_2.tap_on!(stan)
-
-      stan.name
-
-      expect(count_1).to eq(1)
-      expect(count_2).to eq(-1)
-    end
-    it "tracks alias" do
-      c = Class.new(Student)
-      c.class_eval do
-        alias :alias_name :name
-      end
-      stan = c.new("Stan", 18)
-
-      names = []
-
-      device.set_block do |payload|
-        names << payload[:method_name]
-      end
-
-      device.tap_on!(stan)
-
-      stan.alias_name
-
-      expect(names).to match_array([:alias_name])
-    end
-
-    context "when targets are ActiveRecord::Base instances" do
-      context "with track_as_records: true" do
-        it "tracks ActiveRecord::Base instances with their ids" do
-          device = described_class.new(exclude_by_paths: [/gems/], track_as_records: true)
-          post = Post.create!(title: "foo", content: "bar")
-
-          device.tap_on!(post)
-
-          Post.last.title
-
-          expect(device.calls.count).to eq(1)
-        end
-      end
-      context "without track_as_records: true" do
-        it "treats the record like normal objects" do
-          device = described_class.new(exclude_by_paths: [/gems/])
-          post = Post.create!(title: "foo", content: "bar")
-
-          device.tap_on!(post)
-
-          Post.last.title
-
-          expect(device.calls.count).to eq(0)
-        end
-      end
-    end
-
-    describe "yield parameters" do
-      it "detects correct arguments" do
-        stan = Student.new("Stan", 18)
-
-        arguments = []
-
-        device.set_block do |payload|
-          arguments = payload[:arguments]
-        end
-
-        device.tap_on!(stan)
-
-        stan.age = 25
-
-        expect(arguments).to eq({ age: 25 })
-      end
-      it "returns correct filepath and line number" do
-        stan = Student.new("Stan", 18)
-
-        filepath = ""
-        line_number = 0
-
-        device.set_block do |payload|
-          filepath = payload[:filepath]
-          line_number = payload[:line_number]
-        end
-
-        device.tap_on!(stan)
-
-        line_mark = __LINE__
-        stan.age
-
-        expect(filepath).to eq(__FILE__)
-        expect(line_number).to eq((line_mark+1).to_s)
-      end
-    end
-
-    describe "options - with_trace_to: 5" do
-      it "stores trace until given index" do
-        stan = Student.new("Stan", 18)
-
-        device = described_class.new(with_trace_to: 5)
-        device.tap_on!(stan)
-
-        stan.name
-
-        expect(device.calls.first.trace.length).to eq(6)
-      end
-    end
-    describe "options - exclude_by_paths: [/path/]" do
-      it "skips calls that matches the pattern" do
-        stan = Student.new("Stan", 18)
-        count = 0
-
-        device = described_class.new(exclude_by_paths: [/spec/]) { count += 1 }
-        device.tap_on!(stan)
-
-        stan.name
-
-        expect(count).to eq(0)
-      end
-    end
-    describe "options - filter_by_paths: [/path/]" do
-      it "skips calls that matches the pattern" do
-        stan = Student.new("Stan", 18)
-        count = 0
-
-        device_1 = described_class.new(filter_by_paths: [/lib/]) { count += 1 }
-        device_1.tap_on!(stan)
-
-        stan.name
-        expect(count).to eq(0)
-
-        device_2 = described_class.new(filter_by_paths: [/spec/]) { count += 1 }
-        device_2.tap_on!(stan)
-
-        stan.name
-        expect(count).to eq(1)
-      end
-    end
-  end
-
   describe "#and_print" do
     let(:device) { described_class.new }
 
