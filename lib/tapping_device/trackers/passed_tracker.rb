01b9c3d08f4f667b2885caf8a0b6a63e5d56f9ff
st0012 <stan001212@gmail.com>
2020-06-07 17:18:32 +0800

    Simplify PassedTracker's filter condition

--- lib/tapping_device/trackers/passed_tracker.rb
+++ lib/tapping_device/trackers/passed_tracker.rb
@@ -3,10 +3,6 @@ class TappingDevice
     # PassedTracker tracks calls that use the target object as an argument
     class PassedTracker < TappingDevice
       def filter_condition_satisfied?(tp)
-        # we don't care about calls from the device instance or helper methods
-        return false if is_from_target?(tp)
-        return false if tp.defined_class == TappingDevice::Trackable || tp.defined_class == TappingDevice
-
         collect_arguments(tp).values.any? do |value|
           # during comparison, Ruby might perform data type conversion like calling `to_sym` on the value
           # but not every value supports every conversion methods
