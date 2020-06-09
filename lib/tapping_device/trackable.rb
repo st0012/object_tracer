9709ecd0073a83d6dc2b524a7ea47db570ae0493
st0012 <stan001212@gmail.com>
2020-06-09 14:20:07 +0800

    Refactor Trackable module

--- lib/tapping_device/trackable.rb
+++ lib/tapping_device/trackable.rb
@@ -21,35 +21,38 @@ class TappingDevice
     end
 
     def print_traces(target, options = {})
+      output_options = extract_output_options(options)
       options[:event_type] = :call
-      inspect = options.delete(:inspect)
-      colorize = options.fetch(:colorize, true)
 
       device_1 = tap_on!(target, options).and_print do |output_payload|
-        "Called #{output_payload.method_name_and_location(inspect: inspect, colorize: colorize)}"
+        "Called #{output_payload.method_name_and_location(output_options)}"
       end
       device_2 = tap_passed!(target, options).and_print do |output_payload|
-        output_payload.passed_at(inspect: inspect, colorize: colorize)
+        output_payload.passed_at(output_options)
       end
       CollectionProxy.new([device_1, device_2])
     end
 
     def print_calls(target, options = {})
-      inspect = options.delete(:inspect)
-      colorize = options.fetch(:colorize, true)
+      output_options = extract_output_options(options)
 
       tap_on!(target, options).and_print do |output_payload|
-        output_payload.detail_call_info(inspect: inspect, colorize: colorize)
+        output_payload.detail_call_info(output_options)
       end
     end
 
     def print_mutations(target, options = {})
-      inspect = options.delete(:inspect)
-      colorize = options.fetch(:colorize, true)
+      output_options = extract_output_options(options)
 
       tap_mutation!(target, options).and_print do |output_payload|
-        output_payload.call_info_with_ivar_changes(inspect: inspect, colorize: colorize)
+        output_payload.call_info_with_ivar_changes(output_options)
       end
+    end
+
+    private
+
+    def extract_output_options(options)
+      {inspect: options.delete(:inspect), colorize: options.fetch(:colorize, true)}
     end
 
     class CollectionProxy
