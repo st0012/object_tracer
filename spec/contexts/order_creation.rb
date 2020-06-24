203b9b04e00966656ae377798429e4000bcf0846
st0012 <stan001212@gmail.com>
2020-06-24 11:21:07 +0800

    Refactor Trackable's output helper specs

---
+++ spec/contexts/order_creation.rb
@@ -0,0 +1,33 @@
+RSpec.shared_context "order creation" do
+  class Promotion; end
+  class Order;end
+  class Cart
+    def total
+      10
+    end
+    def promotion
+      Promotion.new
+    end
+  end
+  class CartOperationService
+    def perform(cart)
+      validate_cart(cart)
+      apply_discount(cart)
+      create_order(cart)
+    end
+
+    def validate_cart(cart)
+      cart.total
+      cart
+    end
+
+    def apply_discount(cart)
+      cart.promotion
+      cart
+    end
+
+    def create_order(cart)
+      Order.new
+    end
+  end
+end
--- spec/trackable/output_helpers_spec.rb
+++ spec/trackable/output_helpers_spec.rb
@@ -1,69 +1,37 @@
 require "spec_helper"
+require "contexts/order_creation"
 
 RSpec.describe TappingDevice::Trackable do
-  shared_context "order creation" do
-    class Promotion; end
-    class Order;end
-    class Cart
-      def total
-        10
-      end
-      def promotion
-        Promotion.new
-      end
-    end
-    class CartOperationService
-      def perform(cart)
-        validate_cart(cart)
-        apply_discount(cart)
-        create_order(cart)
-      end
-
-      def validate_cart(cart)
-        cart.total
-        cart
-      end
-
-      def apply_discount(cart)
-        cart.promotion
-        cart
-      end
-
-      def create_order(cart)
-        Order.new
-      end
-    end
-  end
-
   let(:cart) { Cart.new }
   let(:service) { CartOperationService.new }
 
   shared_examples "output calls examples" do |action:|
     let(:helper_method) { "#{action}_calls" }
+    let(:tap_action) { send(helper_method, service, colorize: false) }
     let(:expected_output) do
 /:validate_cart # CartOperationService
-    from: #{__FILE__}:.*
+    from: .*:.*
     <= {cart: #<Cart:.*>}
     => #<Cart:.*>
 
 :apply_discount # CartOperationService
-    from: #{__FILE__}:.*
+    from: .*:.*
     <= {cart: #<Cart:.*>}
     => #<Cart:.*>
 
 :create_order # CartOperationService
-    from: #{__FILE__}:.*
+    from: .*:.*
     <= {cart: #<Cart:.*>}
     => #<Order:.*>
 
 :perform # CartOperationService
-    from: #{__FILE__}:.*
+    from: .*:.*
     <= {cart: #<Cart:.*>}
     => #<Order:.*>/
     end
 
     it "prints out target's calls in detail" do
-      send(helper_method, service, colorize: false)
+      tap_action
 
       expect { service.perform(cart) }.to produce_expected_output(expected_output)
     end
@@ -71,12 +39,12 @@ RSpec.describe TappingDevice::Trackable do
     context "with '.with' chained" do
       let(:expected_output) do
 /:create_order # CartOperationService
-    from: #{__FILE__}:.*
+    from: .*:.*
     <= {cart: #<Cart:.*>}
     => #<Order:.*>/
       end
       it "only prints the calls that matches the with condition" do
-        send(helper_method, service, colorize: false).with do |payload|
+        tap_action.with do |payload|
           payload.method_name.to_s.match?(/order/)
         end
 
@@ -87,32 +55,33 @@ RSpec.describe TappingDevice::Trackable do
 
   shared_examples "output traces examples" do |action:|
     let(:helper_method) { "#{action}_traces" }
+    let(:tap_action) { send(helper_method, cart, colorize: false) }
 
     let(:expected_output) do
-/Passed as :cart in 'CartOperationService#:perform' at #{__FILE__}:\d+
-Passed as :cart in 'CartOperationService#:validate_cart' at #{__FILE__}:\d+
-Called :total from: #{__FILE__}:\d+
-Passed as :cart in 'CartOperationService#:apply_discount' at #{__FILE__}:\d+
-Called :promotion from: #{__FILE__}:\d+
-Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
+/Passed as :cart in 'CartOperationService#:perform' at .*:\d+
+Passed as :cart in 'CartOperationService#:validate_cart' at .*:\d+
+Called :total from: .*:\d+
+Passed as :cart in 'CartOperationService#:apply_discount' at .*:\d+
+Called :promotion from: .*:\d+
+Passed as :cart in 'CartOperationService#:create_order' at .*:\d+/
     end
 
     it "prints out what the target sees" do
-      send(helper_method, cart, colorize: false)
+      tap_action
 
       expect { service.perform(cart) }.to produce_expected_output(expected_output)
     end
 
     context "when chained with .with" do
       let(:expected_output) do
-/Passed as :cart in 'CartOperationService#:perform' at #{__FILE__}:\d+
-Passed as :cart in 'CartOperationService#:validate_cart' at #{__FILE__}:\d+
-Passed as :cart in 'CartOperationService#:apply_discount' at #{__FILE__}:\d+
-Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
+/Passed as :cart in 'CartOperationService#:perform' at .*:\d+
+Passed as :cart in 'CartOperationService#:validate_cart' at .*:\d+
+Passed as :cart in 'CartOperationService#:apply_discount' at .*:\d+
+Passed as :cart in 'CartOperationService#:create_order' at .*:\d+/
       end
 
       it "filters output according to the condition" do
-        send(helper_method, cart, colorize: false).with do |trace|
+        tap_action.with do |trace|
           trace.arguments.keys.include?(:cart)
         end
 
@@ -122,8 +91,9 @@ Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
   end
 
   shared_examples "output mutations examples" do |action:|
-    let(:helper_method) { "#{action}_mutations" }
     let(:student) { Student.new("Stan", 26) }
+    let(:helper_method) { "#{action}_mutations" }
+    let(:tap_action) { send(helper_method, student, colorize: false) }
 
     class Student
       def remove_id
@@ -140,27 +110,27 @@ Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
 
     it "tracks attr_writer as well" do
       expected_output = /:name= # #<Class:#<Student:\w+>>
-    from: #{__FILE__}:\d+
+    from: .*:\d+
     changes:
       @name: "Stan" => "Sean"/
 
-      send(helper_method, student, colorize: false)
+      tap_action
 
       expect { student.name = "Sean" }.to produce_expected_output(expected_output)
     end
 
     it "prints calls that define/undefine an object's instance variables" do
       expected_output = /:id= # Student
-    from: #{__FILE__}:\d+
+    from: .*:\d+
     changes:
       @id: \[undefined\] => 1
 
 :remove_id # Student
-    from: #{__FILE__}:\d+
+    from: .*:\d+
     changes:
       @id: 1 => \[undefined\].*/
 
-      send(helper_method, student, colorize: false)
+      tap_action
 
       expect do
         student.id = 1
@@ -170,16 +140,16 @@ Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
 
     it "remembers changed value" do
       expected_output = /:id= # Student
-    from: #{__FILE__}:.*
+    from: .*:.*
     changes:
       @id: \[undefined\] => 1
 
 :id= # Student
-    from: #{__FILE__}:.*
+    from: .*:.*
     changes:
       @id: 1 => nil/
 
-      send(helper_method, student, colorize: false)
+      tap_action
 
       expect do
         student.id = 1
@@ -190,20 +160,20 @@ Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
 
     it "tracks multiple levels of state changes" do
       expected_output = /:age= # Student
-    from: #{__FILE__}:.*
+    from: .*:.*
     changes:
       @age: 26 => 0
 
 :reset_data! # Student
-    from: #{__FILE__}:.*
+    from: .*:.*
     changes:
       @name: "Stan" => ""
       @age: 26 => 0
       @id: 1 => 0/
 
-      student.id = 1
+      tap_action
 
-      send(helper_method, student, colorize: false)
+      student.id = 1
 
       expect { student.reset_data! }.to produce_expected_output(expected_output)
     end
@@ -230,26 +200,8 @@ Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
   end
 
   describe "write_* helpers" do
-    let(:output_log_file) { "/tmp/tapping_device.log" }
-
     def produce_expected_output(log_file = output_log_file, expected_output)
       write_to_file(log_file, expected_output)
-    end
-
-    it "writes to designated file if log_file is provided" do
-      student = Student.new("Stan", 26)
-      log_file = "/tmp/another_file.log"
-
-      expected_output = /:name= # #<Class:#<Student:\w+>>
-    from: #{__FILE__}:\d+
-    changes:
-      @name: "Stan" => "Sean"/
-
-      write_mutations(student, log_file: log_file, colorize: false)
-
-      expect { student.name = "Sean" }.to produce_expected_output(log_file, expected_output)
-
-      File.delete(log_file)
     end
 
     describe "#write_calls" do
@@ -264,6 +216,24 @@ Passed as :cart in 'CartOperationService#:create_order' at #{__FILE__}:\d+/
 
     describe "#write_mutations" do
       it_behaves_like "output mutations examples", action: "write"
+    end
+
+    let(:output_log_file) { "/tmp/tapping_device.log" }
+
+    it "writes to designated file if log_file is provided" do
+      student = Student.new("Stan", 26)
+      log_file = "/tmp/another_file.log"
+
+      expected_output = /:name= # #<Class:#<Student:\w+>>
+    from: .*:\d+
+    changes:
+      @name: "Stan" => "Sean"/
+
+      write_mutations(student, log_file: log_file, colorize: false)
+
+      expect { student.name = "Sean" }.to produce_expected_output(log_file, expected_output)
+
+      File.delete(log_file)
     end
   end
 end
