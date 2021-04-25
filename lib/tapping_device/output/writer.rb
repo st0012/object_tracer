de593e9cc189ef2f09e33d4c2d4bcf83d583d3cf
st0012 <stan001212@gmail.com>
2021-04-25 15:07:13 +0800

    Child writer classes are now redundant

--- lib/tapping_device/output/writer.rb
+++ lib/tapping_device/output/writer.rb
@@ -1,9 +1,10 @@
 class TappingDevice
   module Output
     class Writer
-      def initialize(options, output_block)
+      def initialize(options, output_block, logger:)
         @options = options
         @output_block = output_block
+        @logger = logger
       end
 
       def write!(payload)
